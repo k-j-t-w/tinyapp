8cc26f57f696cc4f86a5b6fd940a963a1cc8c896
Kai Wingfield <kaiwingfield17@gmail.com>
2024-01-10 14:14:52 -0800

    Add a post route for /register. Add new user object. Set user_id cookie. Update all endpoints to pass the users object instead of username value. Update header to show email instead of username

--- express_server.js
+++ express_server.js
@@ -28,13 +28,28 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
 app.get("/register", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    users: users,
+    user_id: req.cookies["user_id"],
+    urls: urlDatabase
   };
   res.render("register", templateVars)
 
@@ -42,7 +57,9 @@ app.get("/register", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    users: users,
+    user_id: req.cookies["user_id"],
+    urls: urlDatabase
   };
   res.render("urls_new", templateVars);
 });
@@ -51,7 +68,9 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]
+    users: users,
+    user_id: req.cookies["user_id"],
+    urls: urlDatabase
   };
   res.render("urls_show", templateVars);
 });
@@ -103,14 +122,25 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 });
 
+app.post('/register', (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  let userN = generateRandomString();
+  const email = req.body.email;
+  const password = req.body.password;
+  users[userN] = {id: userN, email: email, password: password};
+  res.cookie("user_id", userN);
+  res.redirect(`/urls`);
+})
+
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    users: users,
+    user_id: req.cookies["user_id"],
     urls: urlDatabase
   };
   console.log(templateVars);
